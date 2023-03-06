2e9992071adaeeb654502b99f51efc7fb4170c8d
Bruno ILPONSE <b.ilponse@ad.isae-supaero.fr>
2023-03-06 11:49:59 +0100

    Added questions view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def questions(request):
+    questions = Question.objects.all()
+    res = ""
+    for question in questions:
+        res = f"{res}{question.question_text}<br/>"
+
+    return HttpResponse(res)
